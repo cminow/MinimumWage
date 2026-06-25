0199c369703eaddbb4fa7d7ea342f201137a6693
Charlie Minow <minow@mac.com>
2026-06-25 10:44:09 -0600

    Space things out a bit more.

--- Minimum Wage/ContentView.swift
+++ Minimum Wage/ContentView.swift
@@ -11,14 +11,16 @@ struct ContentView: View {
     let timePerPenny: Double = 3600.0 / 725.0
     @State private var runningSimulation: Bool = false
     @State private var earnings: Double = 0.00
-    @State private var lastWorkDate: Date = Date()
+    @State private var startWorkDate: Date = .now
+    @State private var lastWorkDate: Date = .now
     @State private var disableWorkButton: Bool = false
 
     var body: some View {
         VStack(spacing: 8.0) {
-            TimelineView(.periodic(from: .now, by: 0.1)) { context in
+            TimelineView(.periodic(from: .now, by: 0.01)) { context in
                 Text("Earnings: \(earnings.formatted(.currency(code: "USD")))")
                     .monospacedDigit()
+                    .padding(20.0)
                     .onChange(of: context.date) { oldDate, newDate in
                         if newDate.timeIntervalSince1970 >= lastWorkDate.timeIntervalSince1970 + timePerPenny {
                             disableWorkButton = false
@@ -33,7 +35,6 @@ struct ContentView: View {
                 earnings += 0.01
             } label: {
                 Text("Do Work")
-                    .foregroundStyle(disableWorkButton ? .gray : .black)
             }
             .padding([.leading, .trailing], 8.0)
             .padding([.top, .bottom], 4.0)
@@ -41,16 +42,18 @@ struct ContentView: View {
                 RoundedRectangle(cornerRadius: 4)
                     .foregroundStyle(disableWorkButton ? Color("disabledGray") : .green)
             }
+            .tint(disableWorkButton ? .gray : .black)
             .disabled(disableWorkButton)
 
             Button {
                 disableWorkButton = true
+                startWorkDate = .now
                 lastWorkDate = .now
                 earnings = 0.00
             } label: {
                 Text("Reset")
             }
-            .padding(10.0)
+            .padding(20.0)
         }
         .padding()
     }
