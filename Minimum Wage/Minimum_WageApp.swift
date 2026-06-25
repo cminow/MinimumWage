3f00aae681c750ec16e4354c27d81a7efac21736
Charlie Minow <minow@mac.com>
2026-06-25 10:02:59 -0600

    Initial Commit

---
+++ Minimum Wage/Minimum_WageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minimum_WageApp.swift
+//  Minimum Wage
+//
+//  Created by Charlie Minow on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Minimum_WageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Minimum WageTests/Minimum_WageTests.swift
@@ -0,0 +1,17 @@
+//
+//  Minimum_WageTests.swift
+//  Minimum WageTests
+//
+//  Created by Charlie Minow on 6/25/26.
+//
+
+import Testing
+@testable import Minimum_Wage
+
+struct Minimum_WageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
